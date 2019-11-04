051f61b71d7ca2c385bc99897173502dc37e1ab2
run-after <run.after@protonmail.com>
2019-11-04 15:13:14 -0600

    computer generates answer, human can guess, and it displays the guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,33 +1,52 @@
+class Board
+  
+    def colors
+      @colors = ['red', 'blue', 'green', 'yellow', 'white', 'black']
+    end
+  
+  end
+
 class Computer
+  #generates a random answer out of the 6 options from Board class
+  def initialize
+    board = Board.new
+    @answer = [board.colors[rand(6)], board.colors[rand(6)], 
+               board.colors[rand(6)], board.colors[rand(6)]]
+  end
   
   def answer
-    @answer = [Board.new.colors[rand(4)], Board.new.colors[rand(4)], 
-               Board.new.colors[rand(4)], Board.new.colors[rand(4)]]
+    @answer
   end
 
 end
 
 class Human
+  
+  def initialize
+    @guess = []
+  end
 
+  def add(value)
+    @guess.push(value)
+  end
+
+  def guess
+    @guess
+  end
 
 end
 
 class Game
-  
-  while round < 13
-    if Human.answer == Computer.answer #almost definetly won't work.
-        puts "YOU WIN!"
-    end
+  human = Human.new
+  4.times do |x| 
+    puts "What's your guess for position number #{x+1}?" 
+    human.add(gets.chomp)
   end
+  puts "Your current guess is: #{human.guess.join(', ')}"
+
 
 end
 
-class Board
-  
-  def colors
-    colors = ['red', 'blue', 'green', 'yellow', 'white', 'black']
-  end
 
-end
 
-p Computer.new.answer
+Game.new
