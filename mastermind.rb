2467acb99d860073d61dc2290fa2580d0748a032
run-after <run.after@protonmail.com>
2019-11-06 20:05:43 -0600

    started adding ability for computer to guess and human to create code

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,39 @@
 class Board
   
     def colors
-      @colors = ['red', 'blue', 'green', 'yellow', 'white', 'black']
+      @COLORS = ['red', 'blue', 'green', 'yellow', 'white', 'black']
+    end
+
+    def size
+        @COLORS.size
     end
     
     def rounds
         @ROUNDS = 12
     end
+
   end
 
-class Computer
+  class Player
+    attr_accessor :guess
+
+    def initialize
+      @guess = []
+    end
+  
+    def add(value)
+      @guess.push(value)
+    end
+  end
+
+class Computer < Player
   #generates a random answer out of the 6 options from Board class
   def initialize
     board = Board.new
-    @answer = [board.colors[rand(6)], board.colors[rand(6)], 
-               board.colors[rand(6)], board.colors[rand(6)]]
+    @answer = [board.colors[rand(board.size)], board.colors[rand(board.size)], 
+    board.colors[rand(board.size)], board.colors[rand(board.size)]]
+    @guess = [board.colors[rand(board.size)], board.colors[rand(board.size)], 
+    board.colors[rand(board.size)], board.colors[rand(board.size)]]
   end
   
   def answer
@@ -23,25 +42,25 @@ class Computer
 
 end
 
-class Human
+class Human < Player
   
-  def initialize
-    @guess = []
-  end
-
-  def add(value)
-    @guess.push(value)
-  end
-
-  attr_accessor :guess
 
 end
 
 class Game
-    board = Board.new
-    computer = Computer.new
-    human = Human.new
-    round = 0
+  board = Board.new
+  computer = Computer.new
+  human = Human.new
+  round = 0
+
+  puts "Would you like to guess or create the secret code?"
+  game_mode = gets.chomp 
+  until game_mode == 'create' || game_mode == 'guess'
+    puts "You must select either 'guess' or 'create'. Try again..."
+    game_mode = gets.chomp
+  end 
+###############guessing mode#######################
+  if game_mode == 'guess'
 
     p computer.answer
 
@@ -56,10 +75,11 @@ class Game
         puts "Your color choices are #{board.colors.join(', ')}."
         puts
     end
-    #resets the counters
+    #resets the counters and human guesses each iteration
     color_counter = 0
     exact_counter = 0
     human.guess = []
+
     comp_tally = Hash.new(0)
     human_tally = Hash.new(0)
     #adds guesses into guess array
@@ -71,17 +91,16 @@ class Game
         temp = gets.chomp
       end
       human.add(temp)
-          
-    
     end
     #re-iterates guesses
     puts
     puts "Your current guess is: #{human.guess.join(', ')}"
     puts
     #checks how many of the colors are correct
-    
+    #creates a tally of both computer answer and player guess
     computer.answer.each { |x| comp_tally[x] +=1 }
     human.guess.each { |y| human_tally[y] += 1 }
+    #compares guesses to answer tallies
       comp_tally.each do |key, value|
         if human_tally.include?(key)
           if value > human_tally[key]
@@ -90,10 +109,7 @@ class Game
             color_counter += value
           end
         end
-
-        end
-    
-
+       end
     puts "You've got #{color_counter} correct guesses for the colors"
     #checks how many are exact matches
     4.times do |x|
@@ -106,6 +122,10 @@ class Game
     
     round += 1
   end
+elsif game_mode == 'create'
+################create mode####################
+    puts "coming soon"
+end
 
 end
 
